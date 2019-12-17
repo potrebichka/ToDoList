c45d9e70c73922f8df1ceb23e5b99bc70d5209be
Misaki Koonce and Trevor Mackin <misaki@>
2019-12-17 10:44:39 -0800

    Instantiate four todo items and check this.

--- ToDoList/Model/Item.cs
+++ ToDoList/Model/Item.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
@@ -6,11 +7,15 @@ namespace ToDoList.Models
   {
     // We add set; to the line below.
     public string Description { get; set; }
+    public string String { get; set; }
+    public int Num { get; set; }
     private static List<Item> _instances = new List<Item> {};
 
-    public Item(string description)
+    public Item(string description, int num)
     {
         Description = description;
+        Num = num;
+        String = "Hello";
         _instances.Add(this);
     }
 
