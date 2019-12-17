9e28c91fc7df58926d24d59be39805a4591fd313
Misaki Koonce and Trevor Mackin <trevor@>
2019-12-17 09:37:11 -0800

    Initialize ToDoList and unit test.

---
+++ ToDoList/ToDoList.cs
@@ -0,0 +1,14 @@
+using System;
+using ToDoList.Models;
+
+namespace ToDoList
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Item item1 = new Item()
+        }
+    }
+}
+
